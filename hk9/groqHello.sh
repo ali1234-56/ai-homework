4cd7de9edb33277136789c4e37d12de598b1de4e
ali1234-56 <a09694269@gmail.com>
2024-06-17 17:01:46 +0800

    homework

---
+++ hk9/groqHello.sh
@@ -0,0 +1,6 @@
+# GROQ_API_KEY=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 python groqChat.py What is GPT?
+GROQ_API_KEY=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 python groqHello.py
+
+# $env:GROQ_API_KEY = "your-api-key-here" 暫時設置 key
+# $env:GROQ_API_KEY 檢查 key
+
