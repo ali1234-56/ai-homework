aa86594044636b7e220dedb5e298d1fc840816c8
ali1234-56 <a09694269@gmail.com>
2024-06-18 00:47:12 +0800

    homework

--- hk10/README.md
+++ hk10/README.md
@@ -1,10 +1,14 @@
-## 使用 langchain 完成 ， 參考官方文件 ，搜索使用 內建 Tavily ， 生成一樣使用 groq
+## 使用 langchain 完成 ，參考官方文件編寫，搜索使用內建的 Tavily ，生成一樣使用 groq ，最後使用 agent 將這些動作結合輸出結果，輸出樣式也是容易觀看
+
+
 
 [langchain文件](https://python.langchain.com/v0.2/docs/introduction/)
 
 [groq文件](https://python.langchain.com/v0.2/docs/integrations/chat/groq/)
 
 [tavily文件](https://python.langchain.com/v0.2/docs/integrations/tools/tavily_search/#chaining)
+
+[agent使用](https://ithelp.ithome.com.tw/articles/10339805)
 
 輸出 :
 
--- hk10/rag.py
+++ hk10/rag.py
@@ -4,7 +4,7 @@ from langchain.agents import AgentExecutor, create_react_agent
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_groq import ChatGroq
 
-
+# 導入 key
 os.environ["TAVILY_API_KEY"] = "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"
 os.environ["LANGCHAIN_API_KEY"] = "sv2_pt_0b5e556536224e12b0b96559693a74aa_64d388f217"
 
@@ -14,13 +14,13 @@ prompt = hub.pull("hwchase17/react")
 
 llm = ChatGroq(api_key=os.environ.get("GROQ_API_KEY"))
 
+# 定義工具列表，這裡包含了 TavilySearchResults
+tools = [TavilySearchResults(max_results=2)] # 返回最多兩個結果
 
-tools = [TavilySearchResults(max_results=2)]
-
-
+# 創建新的 agent，結合了 ChatGroq 模型、工具列表和模型提示
 agent = create_react_agent(llm, tools, prompt)
 
-
+# 由 LLM 和 prompt 所結合的 chain，用於執行代理的操作，根據這些輸入，Agent 會返回下一個要採取的動作或給使用者的最終回應
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
 user_input = input("Enter_question: ")
